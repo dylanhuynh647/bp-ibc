86d4790af09f2e90f5f9bc09448311d46ff08dc9
Qingyao-Liu <qingyao.liu.c@gmail.com>
2026-01-08 21:59:59 -0500

    - Each HTML element with data-editable is now stored as its own key-value pair in local storage. - Tested if the check for local storage being full works. It works. - Packaged the code which loads from local storage as its own file.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,31 +2,15 @@ import './App.css'
 import UIContextProvider from './context/UIContext'
 import SaveButton from './components/save/SaveButton'
 import useLocalAutoSave from './components/save/useLocalAutoSave'
+import LoadFromLocalAutoSave from './components/save/LoadFromLocalAutoSave'
 
 function App() {
-  const showDefaultTemplate : boolean = false; // Debugging purposes
-
   useLocalAutoSave();
-
-  var pageContent : any = "";
-
-  if (localStorage.getItem("Local Autosave Page Content") === null || showDefaultTemplate){
-    pageContent = (
-      <>
-        <span data-editable='true'>Click me! then double-click me!</span>
-        <br></br>
-        <p data-editable='true'>hello</p>
-      </>
-    )
-  }
-  else {
-    pageContent = <div dangerouslySetInnerHTML={{__html: localStorage.getItem("Local Autosave Page Content") as string}} />
-  }
 
   return (
     <>
       <UIContextProvider>
-        {pageContent}
+        <LoadFromLocalAutoSave/>
       </UIContextProvider>
 
       <br/>
