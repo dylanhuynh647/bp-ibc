6ea6c0a12d43cf0d27283255fc5b16780fb70bc0
Saaj Patel <saaj@uoguelph.ca>
2026-01-26 12:45:17 -0500

    added 'not every animal' component, also created education_page folder in components/site

--- frontend/src/pages/EducationPage.tsx
+++ frontend/src/pages/EducationPage.tsx
@@ -1,15 +1,15 @@
 import Navbar from '../components/site/Navbar'
 import Footer from '../components/site/Footer'
 import WildlifeGuide from '../components/pagesections/WildlifeGuide'
+import NotEveryAnimal from '../components/site/education_page/NotEveryAnimal'
 import EducationQuestions from '../components/site/EducationQuestions'
 
 function EducationPage() {
     return (
         <>
             <Navbar/> 
-            <h1>Welcome to the Education Page</h1>
-            <p>This is the main content area.</p>
             <WildlifeGuide/>
+            <NotEveryAnimal />
             <EducationQuestions/>
             <Footer/> 
         </>
